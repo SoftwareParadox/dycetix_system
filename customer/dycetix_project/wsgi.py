751ec3863b0630ab73b3ebdf273c4c3162821bc5
SoftwareParadox <13238922wm@gmail.com>
2026-01-09 21:54:46 +0200

    Prepare for Render deployment - simple approach

--- customer/dycetix_project/wsgi.py
+++ customer/dycetix_project/wsgi.py
@@ -14,7 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dycetix_project.settings')
 
 application = get_wsgi_application()
-
-# ADD THIS SECTION FOR WHITENOISE
-from whitenoise import WhiteNoise
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
